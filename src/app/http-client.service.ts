39125b0506e846381e600b8a942e6db1a9a441f3
Guzik27 <dguzikow27@gmail.com>
2020-03-01 15:13:01 +0100

    definicja głównego adresu którym bęziemy się łączyć z naszym serverem definicja nagłówka żądania, czego oczekujemy

--- src/app/http-client.service.ts
+++ src/app/http-client.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import {HttpHandler, HttpHeaders} from "@angular/common/http";
+
+/*definicja głównego adresu którym bęziemy się łączyć z naszym serverem*/
+const BASE_URL='http://localhost:8082/api';
+/*definicja nagłówka żądania, czego oczekujemy*/
+const HTTP_OPTIONS = {headers: new HttpHeaders({'Content-Type': 'application/json'})}
 
 @Injectable({
   providedIn: 'root'
