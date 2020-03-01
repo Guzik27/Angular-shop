96477aa73e2fb6db9fe8fb422dbd87dfe7797313
Guzik27 <dguzikow27@gmail.com>
2020-03-01 11:29:14 +0100

    dodanie dzieci do shop, żebyd ostać się do produktów wpiszemy shop/products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {ShopComponent} from "./shop/shop.component";
+import {ProductsComponent} from "./shop/products/products.component";
+import {ShippingComponent} from "./shop/shipping/shipping.component";
 
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
-  {path: 'shop', component: ShopComponent}
+  {path: 'shop', component: ShopComponent, children: [
+      {path: 'products', component: ProductsComponent},
+      {path: 'shipping', component: ShippingComponent}
+    ]}
 ];
 
 @NgModule({
