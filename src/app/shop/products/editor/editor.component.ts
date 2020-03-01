d0596b6db19a0afd05956cd4e1063ba96a24297e
Guzik27 <dguzikow27@gmail.com>
2020-03-01 14:40:10 +0100

    tworzymy metodę . WYwułuje się przy inicjalizacji edycji produktu

--- src/app/shop/products/editor/editor.component.ts
+++ src/app/shop/products/editor/editor.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Product} from "../Product";
 import {ProductStorageService} from "../../../product-storage.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-editor',
@@ -11,9 +11,10 @@ import {Router} from "@angular/router";
 export class EditorComponent implements OnInit {
 
   /*wstrzykiwanie + po dodaniu przeniesienie do wszystkich produktów*/
-  constructor(private productStorage: ProductStorageService, private router: Router) { }
+  constructor(private productStorage: ProductStorageService, private router: Router, private activeRoute: ActivatedRoute) { } /*wbudowane w angulara do obsługi adresów*/
 
   ngOnInit(): void {
+    this.getProductToEdit();
   }
 
   product: Product = new Product(); /*dzięki temu odnosząc się z formularza do właściwści będziemy go wyświwtlali jako pusty*/
@@ -22,4 +23,10 @@ export class EditorComponent implements OnInit {
     this.productStorage.saveProduct(product);
     this.router.navigate(['/shop'])
   }
+
+  /*tworzymy metodę . WYwułuje się przy inicjalizacji edycji produktu*/
+  getProductToEdit() {
+
+  }
+
 }
